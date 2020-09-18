60861b2ab9cfb5c96aa8337fec2fd1a1c439482a
rahulsinghexp <rschauhan2199@gmail.com>
2020-09-18 19:27:27 +0530

    Network Sniffer Tool (Sniff_Tool.py) added. It uses argparse and scapy Network Sniffer Tool (Sniff_Tool2.py) added. It uses sys and scapy

---
+++ Sniff_Tool.py
@@ -0,0 +1,32 @@
+import sys
+# If using Jupyter Notebook jupyter_argparse.core is another option for you
+import argparse
+from scapy.all import scapy
+
+parser = argparse.ArgumentParser()
+# call it from the command line using root/admin privileges
+# Syntax : python <filename> -ip <IP>/<Subnet>
+# e.g. sudo Sniff_Tool.py -ip 172.16.24.1/24
+# Note : Put your IP address, so that all the responses will be sent to you.
+parser.add_argument("-ip", "--ipadd", help="IP Address/Subnet Mask")
+args = parser.parse_args()
+
+if not args.ipadd:
+    print("Invalid Syntax")
+    print("Use --help or -h for options.")
+    sys.exit(1)
+else:
+    # pdst stands for destination IP address
+    arp_request = scapy.layers.l2.ARP(pdst= args.ipadd)
+    broadcast_frame = scapy.layers.l2.Ether(dst="ff:ff:ff:ff:ff:ff")
+    final_request = broadcast_frame/arp_request
+    results_ans = scapy.layers.l2.srp(final_request, timeout=2, verbose=False)[0]
+    # results_unaswered = scapy.layers.l2.srp(final_request, timeout=2, verbose=False)[1]
+    results = []
+    for i in range(0,len(results_ans)):
+        # psrc stands for source IP Address
+        # hwsrc stands for destination MAC Address
+        clients_data = {"ip":results_ans[i][0].psrc," mac":results_ans[i][1].hwsrc}
+        results.append(clients_data)
+    for i in range(len(results)):
+        print(results[i])
---
+++ Sniff_Tool2.py
@@ -0,0 +1,22 @@
+import sys
+from scapy.all import scapy
+print("Scapy, IP and MAC address Sniffer ",scapy.__version__)
+# Call it from the command line using root/admin privileges
+# Syntax : python <file_name> <ip_address>/<subnet>
+# Note : Put Your IP address, so that all the responses will be sent to you.
+print("IP address Entered", sys.argv[1])
+# pdst stands for destination IP address
+arp_req = scapy.layers.l2.ARP(pdst=sys.argv[1])
+# Making a broadcast frame
+broadcast_frame = scapy.layers.l2.Ether(dst="ff:ff:ff:ff:ff:ff")
+broadcast_frame_with_arp =broadcast_frame / arp_req
+results = scapy.layers.l2.srp(broadcast_frame_with_arp, timeout=1,verbose=True)[0]
+output = []
+for i in range(0, len(results)):
+    # psrc stands for Source IP
+    # hwsrc stands for Destination MAC Address
+    temp = {"ip":results[i][0].psrc,"mac":results[i][1].hwsrc}
+    output.append(temp)
+for i in range(len(output)):
+    print(output[i])
+

be5b0cb0a6a3a9915c359909b4f77854d7dd4671
Rahul Singh Chauhan <71509812+rahulsinghinfosec@users.noreply.github.com>
2020-09-29 19:59:02 +0530

    Update Sniff_Tool.py

--- Sniff_Tool.py
+++ Sniff_Tool.py
@@ -26,7 +26,7 @@ else:
     for i in range(0,len(results_ans)):
         # psrc stands for source IP Address
         # hwsrc stands for destination MAC Address
-        clients_data = {"ip":results_ans[i][0].psrc," mac":results_ans[i][1].hwsrc}
-        results.append(clients_data)
+        clients = {"ip":results_ans[i][1].psrc," mac":results_ans[i][1].hwsrc}
+        results.append(clients)
     for i in range(len(results)):
         print(results[i])
